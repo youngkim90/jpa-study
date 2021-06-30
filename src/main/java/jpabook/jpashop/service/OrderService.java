87885e6af7a3f78fcf7ee0ce08aedc6b58274b25
0kim <ceo.youngkim@gmail.com>
2021-06-30 15:42:21 +0900

    complete

--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -8,6 +8,7 @@ import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import jpabook.jpashop.repository.MemberRepository;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -51,6 +52,7 @@ public class OrderService {
     /**
      * 주문취소
      */
+    @Transactional
     public void cancelOrder(Long orderId) {
         //주문 엔티티 조회
         Order order = orderRepository.findOne(orderId);
@@ -59,5 +61,7 @@ public class OrderService {
     }
 
     //검색
-//    public List<Order> findOrders(OrderSeacrch orderSeacrch){}
+    public List<Order> findOrders(OrderSearch orderSearch){
+        return orderRepository.findAllByString(orderSearch);
+    }
 }
--- src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -6,6 +6,7 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import jpabook.jpashop.repository.OrderRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -16,8 +17,6 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -32,16 +31,9 @@ class OrderServiceTest {
     @Test
     public void 상품주문() throws Exception {
         //given
-        Member member = new Member();
-        member.setName("회원1");
-        member.setAddress(new Address("서울", "경기", "123-123"));
-        em.persist(member);
+        Member member = createMember("회원1");
 
-        Book book = new Book();
-        book.setName("시골 JPA");
-        book.setPrice(10000);
-        book.setStockQuantity(10);
-        em.persist(book);
+        Book book = createBook("시골 JPA", 10000, 10);
 
         int orderCount = 2;
 
@@ -58,13 +50,52 @@ class OrderServiceTest {
     }
 
     @Test
-    public void 주문취소() throws Exception {
+    public void 상품주문_재고수량초과() throws Exception {
 
+        Member member = createMember("회원1");
+        Item item = createBook("시골 JPA", 10000, 10);
+
+        int orderCount = 11;
+
+        Assertions.assertThrows(NotEnoughStockException.class, ()->{orderService.order(member.getId(), item.getId(), orderCount);});
     }
 
     @Test
-    public void 상품주문_재고수량초과() throws Exception {
+    public void 주문취소() throws Exception {
+        //given
+        Member member =  createMember("회원1");
+        Book item = createBook("시골 JPA", 10000, 10);
+        int orderCount = 2;
 
+        Long orderId = orderService.order(member.getId(), item.getId(), orderCount);
+
+        //when
+        orderService.cancelOrder(orderId);
+
+        //then
+        Order getOrder = orderRepository.findOne(orderId);
+
+        Assertions.assertEquals(OrderStatus.CANCEL, getOrder.getStatus(),"주문 취소시 상태는 CANCEL 이다.");
+        Assertions.assertEquals(10, item.getStockQuantity(),"주문이 취소된 상품은 그만큼 재고가 증가해야 한다.");
+
+    }
+
+
+    private Book createBook(String name, int price, int stockQuantity) {
+        Book book = new Book();
+        book.setName(name);
+        book.setPrice(price);
+        book.setStockQuantity(stockQuantity);
+        em.persist(book);
+        return book;
+    }
+
+    private Member createMember(String name) {
+        Member member = new Member();
+        member.setName(name);
+        member.setAddress(new Address("서울", "경기", "123-123"));
+        em.persist(member);
+        return member;
     }
 
 }
